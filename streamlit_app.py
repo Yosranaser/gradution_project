dc9c19fa779e737b482fd43348457cfb79cf7662
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2025-05-23 08:50:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,6 @@ cred = credentials.Certificate("predictive-maintance-data-firebase-adminsdk-fbsv
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://predictive-maintance-data-default-rtdb.firebaseio.com/'
 })
-
 # Read data from Firebase
 fuel = db.reference('fuel_level').get()
 speed = db.reference('speed').get()
