7630da261d817e3e356288643fe13a7b32cd1970
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2025-06-23 17:34:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,6 @@ if uploaded_image is not None:
         flag=1
         st.success("✅ Face matched with shorouk")
         st.image("shorouk2.jpg", caption="shorouk ahmed")
-        cap.release()       
-        cv2.destroyAllWindows()
         
     else:
         st.error("❌ Face not recognized")
