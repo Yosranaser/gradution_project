4106e34a4ecfe275ed2fe047ec5ed876eec79778
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2025-06-27 08:42:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,11 @@ sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=cs
 df = pd.read_csv(sheet_url)
 st.title("📊 بيانات السيارة من Google Sheet")
 st.dataframe(df)
-with open('model (6).pkl', 'rb') as file:
-    model = pickle.load(file)
+
+
+model = xgb.XGBClassifier()
+model.load_model('model.json')
+
 
            
 
